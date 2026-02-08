743f85a65c5be1c98ff671a1c5a2da7753e98b6a
Daniel Contreras <contrerasdaniel142@gmail.com>
2026-02-08 10:25:59 -0500

    correccion enrutamiento

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://metradingplat.com:8080/api',
+  apiUrl: 'https://metradingplat.com/api',
   // Token para autenticación SSE
   // IMPORTANTE: Cambiar este valor en el servidor usando variable de entorno SSE_AUTH_TOKEN
   sseAuthToken: 'CHANGE_THIS_IN_PRODUCTION'
